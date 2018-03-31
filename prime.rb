4e391d5fb33f52f3376dc235135dbee98c7cca64
Shawn Koski <shawnnkoski@gmail.com>
2018-03-31 23:43:52 +0000

    Method to test if a number is prime

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(number)
+ test_number = 2
+ 
+ if number <= 1
+   return false
+ end
+ 
+ while test_number < number do
+   if number % test_number == 0
+     return false
+   else
+     test_number += 1
+   end
+ end
+ true
+end
+
