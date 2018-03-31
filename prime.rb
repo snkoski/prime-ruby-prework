c17766d52aae5587b4825e78371b73c0ebf574ab
Shawn Koski <shawnnkoski@gmail.com>
2018-03-31 23:39:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,13 @@
 # Add  code here!
 def prime?(number)
- if number < 0
-   return false
+ test_number = 2
+ 
+ while test_number < number do
+   if number % test_number == 0
+     return true
+   else
+     test_number += 1
+ end
+ false
 end
 
