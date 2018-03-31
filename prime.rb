ddf23c7fb6b66a78d14b67c32790a4513ddbcfcc
Shawn Koski <shawnnkoski@gmail.com>
2018-03-31 23:34:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,6 @@
 # Add  code here!
 def prime?(number)
- 
- test_array = []
- index = 0
- 
- while index < number do
-   test_array << index
-   index += 1
- end
- 
- 
- 
+ if number < 0
+   return false
 end
 
